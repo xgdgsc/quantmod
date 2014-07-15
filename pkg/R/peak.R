bcb0dc9cc74ef6f5443ba6f73496a1a94cf7a25e
xgdgsc <xgdgsc@gmail.com>
2014-07-15 21:04:56 +0800

    fix findValley no result bug

--- pkg/R/peak.R
+++ pkg/R/peak.R
@@ -15,7 +15,7 @@ findValleys <-
 function(x, thresh=0) {
   pks <- which(diff(sign(diff(x, na.pad=FALSE)),na.pad=FALSE) > 0) + 2
   if( !missing(thresh) ) {
-   pks[x[pks-1]-coredata(x[pks]) > thresh]
+   pks[x[pks-1]-coredata(x[pks]) < thresh]
   } else pks
 }
 
